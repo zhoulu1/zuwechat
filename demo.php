8192c95c5b0a47bd7fb3e6f9afd2d183c3ba1584
unknown <847285370@qq.com>
2022-06-15 17:44:25 +0800

    first

---
+++ demo.php
@@ -0,0 +1,5 @@
+<?php
+require_once('./vendor/autoload.php');
+
+use Zhoul\Zuwechat\Wechat\Hello;
+Hello::say();
---
+++ src/Wechat/Hello.php
@@ -0,0 +1,9 @@
+<?php
+namespace Zhoul\Zuwechat\Wechat;
+
+class Hello{
+
+    public static function say(){
+        echo "Hello World!";
+    }
+}
